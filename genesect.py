160ff0c34620b43e40bbad22357be45e63a758b8
marian <yuzuvulpes@gmail.com>
2023-08-24 08:58:41 -0300

    Add files via upload

--- genesect.py
+++ genesect.py
@@ -101,12 +101,11 @@ class organism(): # Classe que armazenará todas as informações dos indivíduo
 
 
     # UPDATE posição
-    def update_pos(self, settings):
-        dx = self.v * cos(radians(self.r)) * settings['dt']
-        dy = self.v * sin(radians(self.r)) * settings['dt']
+    def update_pos(self, settings): # Atualiza a posição com base na velocidade e orientação
+        dx = self.v * cos(radians(self.r)) * settings['dt'] # Multiplica velocidade pelo radiano do cosseno e pelo intervalo de tempo
+        dy = self.v * sin(radians(self.r)) * settings['dt'] # Multiplica velocidade pelo radiano do seno e pelo intervalo de tempo
         self.x += dx
         self.y += dy
-
 
 
 #--- Funções ---#
@@ -151,76 +150,6 @@ def plot_frame(settings, organisms, foods, gen, time):
     # Salva a figura
     plt.savefig(str(gen)+'-'+str(time)+'.png', dpi=100)
 ##    plt.show()
-
-
-def evolve(settings, organisms_old, gen):
-
-    elitism_num = int(floor(settings['elitism'] * settings['pop_size']))
-    new_orgs = settings['pop_size'] - elitism_num
-
-    #--- Pega os status da geração atual
-    stats = defaultdict(int)
-    for org in organisms_old:
-        if org.fitness > stats['BEST'] or stats['BEST'] == 0:
-            stats['BEST'] = org.fitness
-
-        if org.fitness < stats['WORST'] or stats['WORST'] == 0:
-            stats['WORST'] = org.fitness
-
-        stats['SUM'] += org.fitness
-        stats['COUNT'] += 1
-
-    stats['AVG'] = stats['SUM'] / stats['COUNT']
-
-
-    #--- Elitismo (manterá os individuos com melhor performance)
-    orgs_sorted = sorted(organisms_old, key=operator.attrgetter('fitness'), reverse=True)
-    organisms_new = []
-    for i in range(0, elitism_num):
-        organisms_new.append(organism(settings, wih=orgs_sorted[i].wih, who=orgs_sorted[i].who, name=orgs_sorted[i].name))
-
-
-    # Geração de novos organismos
-    for w in range(0, new_orgs):
-
-        # Seleção (truncamento)
-        canidates = range(0, elitism_num)
-        random_index = sample(canidates, 2)
-        org_1 = orgs_sorted[random_index[0]]
-        org_2 = orgs_sorted[random_index[1]]
-
-        # Crossover: selecionar individuos da selação anterior para gerar um novo conjunto de individuos misturando suas caracteristicas
-        crossover_weight = random()
-        wih_new = (crossover_weight * org_1.wih) + ((1 - crossover_weight) * org_2.wih)
-        who_new = (crossover_weight * org_1.who) + ((1 - crossover_weight) * org_2.who)
-
-        # Mutação: A mutação ocorre caso o valor de mutação do individuo seja menor que o limite.
-        # A mutação fará com que uma das duas matrizes de peso da rede neural será substituído por um valor aleatório que esteja dentro de +/- 10% do valor original.
-        # Isso criará a possibilidade de gerar um individuo mais apto, porém impedindo uma mutação brusca para não causar falhas as redes neurais.
-        mutate = random()
-        if mutate <= settings['mutate']:
-
-            # Escolhe a matriz que será modificada
-            mat_pick = randint(0,1)
-
-            # Mutação: peso WIH
-            if mat_pick == 0:
-                index_row = randint(0,settings['hnodes']-1)
-                wih_new[index_row] = wih_new[index_row] * uniform(0.9, 1.1)
-                if wih_new[index_row] >  1: wih_new[index_row] = 1
-                if wih_new[index_row] < -1: wih_new[index_row] = -1
-
-            # Mutação: peso WHO
-            if mat_pick == 1:
-                index_row = randint(0,settings['onodes']-1)
-                index_col = randint(0,settings['hnodes']-1)
-                who_new[index_row][index_col] = who_new[index_row][index_col] * uniform(0.9, 1.1)
-                if who_new[index_row][index_col] >  1: who_new[index_row][index_col] = 1
-                if who_new[index_row][index_col] < -1: who_new[index_row][index_col] = -1
-
-        organisms_new.append(organism(settings, wih=wih_new, who=who_new, name='gen['+str(gen)+']-org['+str(w)+']'))
-
-    return organisms_new, stats
 
 
 def simulate(settings, organisms, foods, gen):
@@ -273,6 +202,77 @@ def simulate(settings, organisms, foods, gen):
     return organisms
 
 
+def evolve(settings, organisms_old, gen):
+
+    elitism_num = int(floor(settings['elitism'] * settings['pop_size'])) # Calcula o número de individuos selecionados pelo elitismo
+    new_orgs = settings['pop_size'] - elitism_num # Calcula o número de seres restantes para completar a próxima geração
+
+    #--- Pega a avaliação da geração atual
+    stats = defaultdict(int)
+    for org in organisms_old:
+        if org.fitness > stats['BEST'] or stats['BEST'] == 0:
+            stats['BEST'] = org.fitness
+
+        if org.fitness < stats['WORST'] or stats['WORST'] == 0:
+            stats['WORST'] = org.fitness
+
+        stats['SUM'] += org.fitness
+        stats['COUNT'] += 1
+
+    stats['AVG'] = stats['SUM'] / stats['COUNT'] # Cria o array de média dos organismos
+
+
+    #--- Elitismo (manterá os individuos com melhor performance)
+    orgs_sorted = sorted(organisms_old, key=operator.attrgetter('fitness'), reverse=True)
+    organisms_new = []
+    for i in range(0, elitism_num):
+        organisms_new.append(organism(settings, wih=orgs_sorted[i].wih, who=orgs_sorted[i].who, name=orgs_sorted[i].name)) # Adiciona os melhores organismos ao array utilizando elitismo
+
+
+    # Geração de novos organismos
+    for w in range(0, new_orgs):
+
+        # Seleção (truncamento)
+        canidates = range(0, elitism_num)
+        random_index = sample(canidates, 2)
+        org_1 = orgs_sorted[random_index[0]]
+        org_2 = orgs_sorted[random_index[1]]
+
+        # Crossover: selecionar individuos da selação anterior para gerar um novo conjunto de individuos misturando suas caracteristicas
+        crossover_weight = random()
+        wih_new = (crossover_weight * org_1.wih) + ((1 - crossover_weight) * org_2.wih)
+        who_new = (crossover_weight * org_1.who) + ((1 - crossover_weight) * org_2.who)
+
+        # Mutação: A mutação ocorre caso o valor de mutação do individuo seja menor que o limite.
+        # A mutação fará com que uma das duas matrizes de peso da rede neural será substituído por um valor aleatório que esteja dentro de +/- 10% do valor original.
+        # Isso criará a possibilidade de gerar um individuo mais apto, porém impedindo uma mutação brusca para não causar falhas as redes neurais.
+        mutate = random()
+        if mutate <= settings['mutate']:
+
+            # Escolhe a matriz que será modificada
+            mat_pick = randint(0,1)
+
+            # Mutação: peso WIH
+            if mat_pick == 0:
+                index_row = randint(0,settings['hnodes']-1)
+                wih_new[index_row] = wih_new[index_row] * uniform(0.9, 1.1)
+                if wih_new[index_row] >  1: wih_new[index_row] = 1
+                if wih_new[index_row] < -1: wih_new[index_row] = -1
+
+            # Mutação: peso WHO
+            if mat_pick == 1:
+                index_row = randint(0,settings['onodes']-1)
+                index_col = randint(0,settings['hnodes']-1)
+                who_new[index_row][index_col] = who_new[index_row][index_col] * uniform(0.9, 1.1)
+                if who_new[index_row][index_col] >  1: who_new[index_row][index_col] = 1
+                if who_new[index_row][index_col] < -1: who_new[index_row][index_col] = -1
+
+        organisms_new.append(organism(settings, wih=wih_new, who=who_new, name='gen['+str(gen)+']-org['+str(w)+']'))
+
+    return organisms_new, stats # Retorna a nova população
+
+
+
 #--- Main ---#
 
 def run(settings):
@@ -301,6 +301,7 @@ def run(settings):
         print('> GEN:',gen,'BEST:',stats['BEST'],'AVG:',stats['AVG'],'WORST:',stats['WORST'])
 
     pass
+
 
 
 run(settings)
